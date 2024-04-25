224fd7b5dc9793edc6afc450653697db80bde45b
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-04-25 14:03:27 +0000

    view&delete_records

--- website/views.py
+++ website/views.py
@@ -48,3 +48,27 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		# look up record
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+	else:
+		messages.success(request, "You mus be logged in to see this page...")
+		return redirect('home')
+	
+
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record deleted successfully...")
+		return redirect('home')
+	else:
+		messages.success(request, "You mus be logged in to do that...")
+		return redirect('home')
+
+
+def add_record(request):
+	return render(request, 'add_record.html', {})
